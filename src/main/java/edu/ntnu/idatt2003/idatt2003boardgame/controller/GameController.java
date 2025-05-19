faeffe0baa42b90c61ab3be57b1345e67588f81d
Zomeiro <ba.vangen5@gmail.com>
2025-05-19 22:09:17 +0200

    Fix: Compatibility with new observer and facade structure

--- src/main/java/edu/ntnu/idatt2003/idatt2003boardgame/controller/GameController.java
+++ src/main/java/edu/ntnu/idatt2003/idatt2003boardgame/controller/GameController.java
@@ -1,4 +1,5 @@
 package edu.ntnu.idatt2003.idatt2003boardgame.controller;
+
 import java.util.List;
 
 import edu.ntnu.idatt2003.idatt2003boardgame.model.Board;
@@ -6,10 +7,13 @@ import edu.ntnu.idatt2003.idatt2003boardgame.model.Player;
 import edu.ntnu.idatt2003.idatt2003boardgame.model.Tile;
 import edu.ntnu.idatt2003.idatt2003boardgame.model.dice.Dice;
 import edu.ntnu.idatt2003.idatt2003boardgame.util.LoopingIterator;
+import edu.ntnu.idatt2003.idatt2003boardgame.model.observer.GameModel;
+import edu.ntnu.idatt2003.idatt2003boardgame.model.observer.GameModelObserver;
 
 public class GameController {
 
-    private VisualController visualController;
+    private GameViewController gameViewController;
+    private final GameModel gameModel = new GameModel();
 
     private final Board board;
     private final List<Tile> tiles;
@@ -17,8 +21,9 @@ public class GameController {
     private final Dice dice;
     private Player playerWhoseTurn;
     private final LoopingIterator<Player> playerIterator;
-;
     private Player playerToSkip = null;
+    private Player winner = null;
+    private boolean finished = false;
 
     public GameController(Board board, List<Player> players) {
         this.board = board;
@@ -27,42 +32,51 @@ public class GameController {
         this.playerIterator = new LoopingIterator<>(players);
         this.dice = new Dice(1);
         this.playerWhoseTurn = playerIterator.next();
-
     }
 
-    public void start() {    
+    public void start() {
         for (Player player : players) {
             board.getTiles().get(0).addPlayer(player);
         }
-
+        gameModel.fireTurnChanged(playerWhoseTurn);
     }
 
     public void movePlayer(Player player, int tileNumber) {
         tiles.get(player.getPosition() - 1).popPlayer();
-
         player.setPosition(tileNumber);
         Tile targetTile = tiles.get(tileNumber - 1);
         targetTile.addPlayer(player);
-
-        if (!(targetTile.getEffect() == null)) {
+        gameModel.firePlayerMoved(player, tileNumber);
+        if (targetTile.getEffect() != null) {
             targetTile.getEffect().execute(player, this);
         }
-
-        visualController.updateEntireBoard();
     }
 
     public void moveBy(Player player, int steps) {
-        int nextPosition = player.getPosition() + steps;
-        movePlayer(player, nextPosition);
-
+        movePlayer(player, player.getPosition() + steps);
     }
 
     public void handleRollDice() {
         int diceRoll = dice.roll();
-        moveBy(playerWhoseTurn, diceRoll); 
-        visualController.displayRoll(diceRoll);
-        advanceTurn();
-        
+        gameModel.fireDiceRolled(diceRoll);
+        moveBy(playerWhoseTurn, diceRoll);
+        if (playerWhoseTurn.getPosition() >= board.getTileCount()) {
+            winner = playerWhoseTurn;
+            finished = true;
+            gameModel.fireGameWon(playerWhoseTurn);
+        }
+        if (!finished) {
+            advanceTurn();
+        }
+    }
+
+    public void advanceTurn() {
+        playerWhoseTurn = playerIterator.next();
+        if (playerToSkip != null && playerToSkip.equals(playerWhoseTurn)) {
+            playerToSkip = null;
+            playerWhoseTurn = playerIterator.next();
+        }
+        gameModel.fireTurnChanged(playerWhoseTurn);
     }
 
     public void markPlayerToSkip(Player player) {
@@ -72,18 +86,28 @@ public class GameController {
     public Player getCurrentPlayer() {
         return playerWhoseTurn;
     }
-    
-    public void advanceTurn() {
-        playerWhoseTurn = playerIterator.next();
 
-        if (playerToSkip != null && playerToSkip.equals(playerWhoseTurn)) {
-            playerToSkip = null;
-            playerWhoseTurn = playerIterator.next();
-        }
+    public GameModel getModel() {
+        return gameModel;
     }
 
-    public void setVisualController(VisualController takenVisualController) {
-        visualController = takenVisualController; 
+    public void setVisualController(GameViewController takenGameViewController) {
+        gameViewController = takenGameViewController;
     }
-    
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public Player getWinner() {
+        return winner;
+    }
+
+    public void addObserver(GameModelObserver observer) {
+        gameModel.addListener(observer);
+    }
+
+    public void removeObserver(GameModelObserver observer) {
+        gameModel.removeListener(observer);
+    }
 }
