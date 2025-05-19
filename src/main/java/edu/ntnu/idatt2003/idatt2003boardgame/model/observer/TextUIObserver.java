d0a5cddee4bf4d0477a2a9e7e265d43e6cc45034
Zomeiro <ba.vangen5@gmail.com>
2025-05-19 22:11:10 +0200

    Feature: added TextUIObserver

---
+++ src/main/java/edu/ntnu/idatt2003/idatt2003boardgame/model/observer/TextUIObserver.java
@@ -0,0 +1,27 @@
+package edu.ntnu.idatt2003.idatt2003boardgame.model.observer;
+
+import edu.ntnu.idatt2003.idatt2003boardgame.model.Player;
+
+
+public class TextUIObserver implements GameModelObserver {
+
+  @Override
+  public void onDiceRolled(int value) {
+    System.out.println("Dice rolled: " + value);
+  }
+
+  @Override
+  public void onPlayerMoved(Player player, int newPosition) {
+    System.out.println(player.getName() + " moved to square: " + newPosition);
+  }
+
+  @Override
+  public void onTurnChanged(Player newPlayer) {
+    System.out.println("It is now " + newPlayer.getName() + "'s turn!");
+  }
+
+  @Override
+  public void onGameWon(Player winner) {
+    System.out.println("Congratulations " + winner.getName() + ", you won the game!");
+  }
+}
