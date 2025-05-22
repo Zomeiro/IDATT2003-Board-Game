fae2700410ca7e52acea7c780ca416b28b4f5964
Zomeiro <ba.vangen5@gmail.com>
2025-05-23 01:45:32 +0200

    Feat: Javadoc and better initialization

--- src/main/java/edu/ntnu/idatt2003/idatt2003boardgame/service/GameInitializationService.java
+++ src/main/java/edu/ntnu/idatt2003/idatt2003boardgame/service/GameInitializationService.java
@@ -1,7 +1,5 @@
 package edu.ntnu.idatt2003.idatt2003boardgame.service;
 
-import java.util.List;
-
 import edu.ntnu.idatt2003.idatt2003boardgame.BoardGame;
 import edu.ntnu.idatt2003.idatt2003boardgame.controller.GameController;
 import edu.ntnu.idatt2003.idatt2003boardgame.controller.GameViewController;
@@ -9,33 +7,54 @@ import edu.ntnu.idatt2003.idatt2003boardgame.model.Board;
 import edu.ntnu.idatt2003.idatt2003boardgame.model.Player;
 import edu.ntnu.idatt2003.idatt2003boardgame.view.elements.BoardView;
 import edu.ntnu.idatt2003.idatt2003boardgame.view.elements.SideColumnView;
-import java.util.function.Consumer;
 import javafx.scene.layout.VBox;
 
+import java.util.List;
+
+/**
+ * Service to hold and provide access to the core components
+ * required for the Ingame scene, based on an initialized BoardGame.
+ */
 public class GameInitializationService {
 
     private final BoardGame boardGame;
     private final SideColumnView sideColumnView;
 
-    public GameInitializationService(String presetName, List<Player> players, Consumer<Player> onGameWon) {
-
-        this.boardGame = BoardGameFactory.createSnakesAndLadders(presetName, players, onGameWon);
-        this.sideColumnView = new SideColumnView(players,boardGame.getGameViewController().getDiceAnimation());
+    /**
+     * Creates a new GameInitializationService.
+     *
+     * @param boardGame The fully initialized BoardGame instance.
+     * @param players   The list of players (needed for SideColumnView).
+     */
+    public GameInitializationService(BoardGame boardGame, List<Player> players) {
+        this.boardGame = boardGame;
+        if (this.boardGame == null || this.boardGame.getGameViewController() == null) {
+            throw new IllegalArgumentException("BoardGame and its ViewController must be initialized.");
+        }
+        this.sideColumnView = new SideColumnView(players, boardGame.getGameViewController().getDiceAnimation());
     }
 
     public Board getBoard() {
         return boardGame.getBoard();
     }
+
     public BoardView getBoardVisual() {
         return boardGame.getBoardView();
     }
+
     public GameController getGameController() {
         return boardGame.getGameController();
     }
+
     public GameViewController getVisualController() {
         return boardGame.getGameViewController();
     }
+
     public VBox getSideColumn() {
         return sideColumnView.getColumn();
+    }
+
+    public BoardGame getBoardGame() {
+        return boardGame;
     }
 }
